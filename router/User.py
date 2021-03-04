5ea67c3dc804f75e7f9ea5c667c1894f23716842
kerryDong11 <55862830+kerryDong11@users.noreply.github.com>
2021-03-04 18:08:00 +0800

    add post put

--- router/User.py
+++ router/User.py
@@ -22,17 +22,29 @@ class User(Resource):
 
 class UserList(Resource):
     def get(self):
-
-        return {'users': [user.to_dict() for user in UserModel.query.all()]}
+        
+        users = [user.to_dict() for user in UserModel.query.filter_by(is_delete = False).all()]
+        for user in users:
+            for k in list(user.keys()):
+                #print (k)
+                if(k == 'is_delete' or k == 'u_password'):  
+                    del user[k]
+        return {'data':users}
+        #return {'data': [user.to_dict() for user in UserModel.query.filter_by(is_delete = False).all()]}
     
     def post(self):
-        # 查询该国家是否存在于数据库中
         #print(json.load(request.json))
         user = UserModel()
         user = user.from_dict(request.json)
-        #print(user.u_email)
+        print(user.u_department)
         db.session.add(user)
         db.session.commit()
         return Success.message, Success.code
 
+    def put (self):
+        username = request.json['username']
+        user = UserModel.query.filter_by(username=username).first()
+        user = user.from_dict(request.json)
+        db.session.commit()
+        return Success.message, Success.code
 
